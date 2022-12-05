e3eab5f7d41d92ccf43f09c2c202d590cdc1ee68
janvi08 <janviphadtare9@gmail.com>
2022-12-05 00:03:02 -0500

    Create profile.php

---
+++ code/profile.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Profile</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/styles.css?x=13">
+        <link rel="stylesheet" href="css/dash_style.css?x=1ac">
+    </head>
+    <body><!-- Navigation -->
+        <nav class="navbar navbar-expand-sm navbar-light navbar-custom">
+            <a class="navbar-brand logo-image" href="index.php"><img src="images/logo.svg" alt="alternative" class="logo-img"></a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarsExampleDefault">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="logout.php">Logout</a>
+                    </li>
+                    <!-- Dropdown Menu -->          
+                    <!--<li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle page-scroll" href="#about" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">About</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#"><span class="item-text">Terms Conditions</span></a>
+                            <div class="dropdown-items-divide-hr"></div>
+                            <a class="dropdown-item" href="#"><span class="item-text">Privacy Policy</span></a>
+                        </div>
+                    </li>-->
+                    <!-- end of dropdown menu -->
+            </ul>
+            </div>
+        </nav> <!-- end of navbar -->
+</body>
+</html>
