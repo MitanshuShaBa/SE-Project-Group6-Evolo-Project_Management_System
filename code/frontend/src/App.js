9e953fde938bc2cea0718cdf0720925dce93b309
MitanshuShaBa <42906426+MitanshuShaBa@users.noreply.github.com>
2022-12-05 16:39:18 -0500

    404 page

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -8,13 +8,29 @@ import Dashboard from "./Components/Dashboard";
 import Footer from "./Components/Footer";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./css/styles.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Profile from "./Components/Profile";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const authStateChange = setInterval(() => {
+      const token = localStorage.getItem("token");
+      if (token !== null) {
+        setIsLogin(true);
+      } else {
+        setIsLogin(false);
+      }
+    }, 500);
+    return () => {
+      clearInterval(authStateChange);
+    };
+    // eslint-disable-next-line
+  }, []);
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar isLogin={isLogin} setIsLogin={setIsLogin} />
 
       <Routes>
         <Route path="/">
@@ -47,15 +63,20 @@ function App() {
               </>
             }
           />
-          <Route
-            path="profile"
-            element={
-              <>
-                <Profile />
-              </>
-            }
-          />
-          <Route path="dashboard" element={<Dashboard />} />
+          {isLogin && (
+            <>
+              <Route
+                path="profile"
+                element={
+                  <>
+                    <Profile />
+                  </>
+                }
+              />
+              <Route path="dashboard" element={<Dashboard />} />
+            </>
+          )}
+          <Route path="*" element={<p>There's nothing here: 404!</p>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- code/frontend/src/Components/Navbar.js
+++ code/frontend/src/Components/Navbar.js
@@ -2,25 +2,9 @@ import logo from "../images/logo.svg";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-const NavBar = () => {
-  const [isLogin, setIsLogin] = useState(false);
+const NavBar = ({ isLogin, setIsLogin }) => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const authStateChange = setInterval(() => {
-      const token = localStorage.getItem("token");
-      const user = JSON.parse(localStorage.getItem("user"));
-      if (token !== null) {
-        setIsLogin(true);
-      } else {
-        setIsLogin(false);
-      }
-    }, 500);
-    return () => {
-      clearInterval(authStateChange);
-    };
-    // eslint-disable-next-line
-  }, []);
   return (
     <nav class="navbar navbar-expand-sm navbar-light navbar-custom">
       <Link to="/" class="navbar-brand logo-image">
