dd905ffb2c785097721127fc6afca38487ecc04b
janvi08 <janviphadtare9@gmail.com>
2022-12-05 00:45:42 -0500

    update profile information

--- code/profile.php
+++ code/profile.php
@@ -1,3 +1,17 @@
+<?php
+    session_start();
+    require_once "pdo.php";
+    $stmt="Select * from user_db where u_id=:user";
+    $stmt=$pdo->prepare($stmt);
+    $stmt->execute(array(
+        "user"=>$_SESSION["u_id"]
+    ));
+    $row=$stmt->fetchALL(PDO::FETCH_ASSOC);
+    $users=array();
+    $users["name"] = $row[0]["name"];
+    $users["contact"] = $row[0]["contact"];
+    $users["email"] = $row[0]["email"];
+?> 
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,7 +19,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Profile</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/styles.css?x=13">
+        <link rel="stylesheet" href="css/styles.css">
         <link rel="stylesheet" href="css/dash_style.css?x=1ac">
     </head>
     <body><!-- Navigation -->
@@ -34,6 +48,31 @@
                     <!-- end of dropdown menu -->
             </ul>
             </div>
-        </nav> <!-- end of navbar -->
+        </nav>
+        <div class="container marginTop">
+        <div class="row"> 
+        <div class="col-lg-12 mb-4 mb-sm-5">
+                <div class="card card-style1 border-0">
+                    <div class="card-body p-1-9 p-sm-2-3 p-md-6 p-lg-7">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 mb-4 mb-lg-0">
+                                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="...">
+                            </div>
+                            <div class="col-lg-6 px-xl-10">
+                                <div class="bg-secondary d-lg-inline-block py-1-9 px-1-9 px-sm-6 mb-1-9 rounded profile-name">
+                                    <h3 class="h2 text-white mb-0"><?echo $users["name"];?></h3>
+                                    <!-- <span class="text-primary">Coach</span> -->
+                                </div>
+                                <ul class="list-unstyled mb-1-9">
+                                    <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Email:</span> <?echo $users["email"];?></li>
+                                    <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Contact:</span> <?echo $users["contact"];?></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</div>
+</div><!-- end of navbar -->
 </body>
 </html>
