93da5ef22105b3c4e6602c4f629e8151b7b33ba9
Mitul Patel <42907640+MitulPatel5522@users.noreply.github.com>
2022-12-05 23:36:53 -0500

    add test case for project routes

--- code/backend/__tests__/index.js
+++ code/backend/__tests__/index.js
@@ -94,3 +94,101 @@ describe("Test the organisation path", () => {
       });
   });
 });
+
+describe("Test the project path", () => {
+  test("It should make an project", (done) => {
+    request(app)
+      .post("/organisation/create")
+      .send({
+        name: "Test Organisation",
+      })
+      .set({
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODY5ZGQzNDY5MGM4MGNjYmM1OTY1ZCIsImVtYWlsIjoibXJlc2hhbUBuY3N1LmVkdSIsImlhdCI6MTY3MDA0MTYxMiwiZXhwIjoxNzAxNTk5MjEyfQ.HpkHvjPWbH4hEcQYRZ3L_x-DFwtOHiG0N7DseB4t8_E `,
+      })
+      .expect(201)
+      .end((_err, res) => {
+        request(app)
+          .post("/project/create")
+          .send({
+            name: "Test Project",
+            organisation: res.body._id,
+          })
+          .set({
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODY5ZGQzNDY5MGM4MGNjYmM1OTY1ZCIsImVtYWlsIjoibXJlc2hhbUBuY3N1LmVkdSIsImlhdCI6MTY3MDA0MTYxMiwiZXhwIjoxNzAxNTk5MjEyfQ.HpkHvjPWbH4hEcQYRZ3L_x-DFwtOHiG0N7DseB4t8_E `,
+          })
+          .expect(201)
+          .expect((res) => res.name === "Test Project")
+          .end(done);
+      });
+  });
+  test("It should update an project", (done) => {
+    request(app)
+      .post("/organisation/create")
+      .send({
+        name: "Test Organisation",
+      })
+      .set({
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODY5ZGQzNDY5MGM4MGNjYmM1OTY1ZCIsImVtYWlsIjoibXJlc2hhbUBuY3N1LmVkdSIsImlhdCI6MTY3MDA0MTYxMiwiZXhwIjoxNzAxNTk5MjEyfQ.HpkHvjPWbH4hEcQYRZ3L_x-DFwtOHiG0N7DseB4t8_E `,
+      })
+      .expect(201)
+      .end((_err, res) => {
+        request(app)
+          .post("/project/create")
+          .send({
+            name: "Test Project",
+            organisation: res.body._id,
+          })
+          .set({
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODY5ZGQzNDY5MGM4MGNjYmM1OTY1ZCIsImVtYWlsIjoibXJlc2hhbUBuY3N1LmVkdSIsImlhdCI6MTY3MDA0MTYxMiwiZXhwIjoxNzAxNTk5MjEyfQ.HpkHvjPWbH4hEcQYRZ3L_x-DFwtOHiG0N7DseB4t8_E `,
+          })
+          .expect(201)
+          .expect((res) => res.name === "Test Project")
+          .end((_err, res) => {
+            request(app)
+              .patch("/project/" + res.body._id)
+              .send({
+                name: "Test Project Change",
+              })
+              .set({
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODY5ZGQzNDY5MGM4MGNjYmM1OTY1ZCIsImVtYWlsIjoibXJlc2hhbUBuY3N1LmVkdSIsImlhdCI6MTY3MDA0MTYxMiwiZXhwIjoxNzAxNTk5MjEyfQ.HpkHvjPWbH4hEcQYRZ3L_x-DFwtOHiG0N7DseB4t8_E `,
+              })
+              .expect(200)
+              .expect((res) => res.name === "Test Project Change")
+              .end(done);
+          });
+      });
+  });
+  test("It should delete an project", (done) => {
+    request(app)
+      .post("/organisation/create")
+      .send({
+        name: "Test Organisation",
+      })
+      .set({
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODY5ZGQzNDY5MGM4MGNjYmM1OTY1ZCIsImVtYWlsIjoibXJlc2hhbUBuY3N1LmVkdSIsImlhdCI6MTY3MDA0MTYxMiwiZXhwIjoxNzAxNTk5MjEyfQ.HpkHvjPWbH4hEcQYRZ3L_x-DFwtOHiG0N7DseB4t8_E `,
+      })
+      .expect(201)
+      .end((_err, res) => {
+        request(app)
+          .post("/project/create")
+          .send({
+            name: "Test Project",
+            organisation: res.body._id,
+          })
+          .set({
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODY5ZGQzNDY5MGM4MGNjYmM1OTY1ZCIsImVtYWlsIjoibXJlc2hhbUBuY3N1LmVkdSIsImlhdCI6MTY3MDA0MTYxMiwiZXhwIjoxNzAxNTk5MjEyfQ.HpkHvjPWbH4hEcQYRZ3L_x-DFwtOHiG0N7DseB4t8_E `,
+          })
+          .expect(201)
+          .expect((res) => res.name === "Test Project")
+          .end((_err, res) => {
+            request(app)
+              .delete("/project/" + res.body._id)
+              .set({
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODY5ZGQzNDY5MGM4MGNjYmM1OTY1ZCIsImVtYWlsIjoibXJlc2hhbUBuY3N1LmVkdSIsImlhdCI6MTY3MDA0MTYxMiwiZXhwIjoxNzAxNTk5MjEyfQ.HpkHvjPWbH4hEcQYRZ3L_x-DFwtOHiG0N7DseB4t8_E `,
+              })
+              .expect(200)
+              .end(done);
+          });
+      });
+  });
+});
