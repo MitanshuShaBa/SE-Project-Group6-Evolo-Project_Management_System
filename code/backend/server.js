95bcf34df7d2eaf31e966e3727863e79751caf16
MitanshuShaBa <42906426+MitanshuShaBa@users.noreply.github.com>
2022-12-05 22:54:59 -0500

    auth org test

---
+++ code/backend/server.js
@@ -0,0 +1,15 @@
+const app = require('.');
+const mongoose = require('mongoose');
+
+//DB connections
+mongoose.connect(process.env.DB_URL).then(() => {
+  console.log('DB CONNECTED!!');
+});
+
+// PORT
+const port = process.env.PORT || 5000;
+
+//Start server
+app.listen(port, () => {
+  console.log(`app is running on port ${port}...`);
+});
